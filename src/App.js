d704a5598663dc82294e81da5ced61a268b2f934
MD MOQADDAS <sgn9556@gmail.com>
2024-08-05 15:25:13 +0530

    need to fix env var

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+
+  function handleOnSubmit(events) {
+    events.preventDefault();
+
+    const name = events.target.elements.name.value;
+    const email = events.target.elements.email.value;
+
+    axios.post( process.env.REACT_APP_BACKEND_URL , ({ name, email })).then((response => {
+      console.log(response.data)
+    })).catch((error) => {
+      console.log(error)
+    })
+  }
+
   return (
-  <div>
-    <h1>Welcome to the ProdSell Intern 😊</h1>
-    <div className='form-style'><input placeholder='Enter Name'  type='text' /></div>
-    <div className='form-style'><input placeholder='Enter Email' type='text'/></div>
-    <div className='form-style'><input type='submit' /></div>
-   </div>
+    <div>
+      <h1>Welcome to the ProdSell Intern 😊</h1>
+
+      <form onSubmit={handleOnSubmit}>
+        <div className='form-style'>
+          <input name='name' placeholder='Enter Name' type='text' />
+        </div>
+        <div className='form-style'>
+          <input name='email' placeholder='Enter Email' type='text' />
+        </div>
+        <div className='form-style'>
+          <input type='submit' />
+        </div>
+      </form>
+
+    </div>
   );
 }
 
